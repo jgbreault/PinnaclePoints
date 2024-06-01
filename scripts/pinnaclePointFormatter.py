67a1a4e603dbd4a37aa07dfb7a496c5ea9f2df05
Jamie Breault <jamiegbreault@gmail.com>
2024-06-01 17:30:53 -0400

    improving mountain names

--- scripts/pinnaclePointFormatter.py
+++ scripts/pinnaclePointFormatter.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import commonFunctions as func
+from bs4 import BeautifulSoup
+import requests
 
 '''
 Gets the prominence of the closest OTOTW to a summit.
@@ -16,7 +18,7 @@ def getClosestOTOTWProminence(summit):
     return closestOTOTW.prominence_m
 
 '''
-Gets the name_local, name_en, and wikipedia info of the closest extremal to a summit.
+Gets the wikipedia info of the closest extremal to a summit within 1 km
 '''
 def getClosestExtremalInfo(summit):
     
@@ -27,26 +29,39 @@ def getClosestExtremalInfo(summit):
                                                                     summit.latitude]), axis=1) 
     if distanceBetweenSummits.min() < 1000:
         closestExtremal = extremals.iloc[distanceBetweenSummits.idxmin()]
-        
-        closestName = closestExtremal['name']
-        closestNameEn = closestExtremal.name_en
+
         closestWiki = closestExtremal.wikipedia
         
-        if pd.isna(closestName) == True:
-            closestName = ''
-        if pd.isna(closestNameEn) == True:
-            closestNameEn = ''
         if pd.isna(closestWiki) == True:
             closestWiki = ''
         
     else:
-        closestName = ''
-        closestNameEn = ''
         closestWiki = ''
         
-    return pd.Series({'name_local': closestName,
-                      'name_en': closestNameEn,
-                      'wikipedia': closestWiki})
+    return closestWiki
+
+'''
+Gets the name of the nearest peak from PeakBagger within 1 km
+'''
+def scrapeMountainName(pinnacle):
+    response = requests.get(f'https://www.peakbagger.com/search.aspx?tid=R&lat={pinnacle.latitude}&lon={pinnacle.longitude}&ss=&u=m')
+    soup = BeautifulSoup(response.text, 'html.parser')
+    
+    newName = None
+    tableTitle = soup.find('h2', text=lambda text: text and 'Radius Search from' in text)
+    if tableTitle:
+        tableRows = tableTitle.find_next('tr').parent.find_all('tr')
+        if len(tableRows) > 2:
+            firstRow = tableRows[1] # need to skip header
+            firstRowCells = firstRow.find_all('td')
+
+            nameCell = firstRowCells[0].text
+            distanceCell = firstRowCells[4].text
+
+            if float(distanceCell) < 1.0:
+                newName = nameCell
+
+    return newName
 
 ototws = pd.read_csv('../dataSources/ototw_p300m.csv')
 extremals = pd.read_csv('../dataSources/formattedExtremals.csv')
@@ -60,19 +75,18 @@ summits = pd.read_csv('../formattedSummits/pinnaclePoints_raw.txt',
                                'elevation', 
                                'h_distance'])
 
+summits['summit_name'] = summits.apply(scrapeMountainName, axis=1)
+
 # convert horizon distance back to prominence
 summits['prominence'] = summits.apply(getClosestOTOTWProminence, axis=1)
 
 # add name/wiki info based on closest extremal point
-summits[['name_local', 'name_en', 'wikipedia']] = summits.apply(getClosestExtremalInfo,
-                                                                axis=1,
-                                                                result_type='expand') 
+summits['wikipedia'] = summits.apply(getClosestExtremalInfo, axis=1) 
 
 summits = summits[['latitude', 
                    'longitude', 
                    'elevation', 
                    'prominence', 
-                   'name_local', 
-                   'name_en', 
+                   'summit_name', 
                    'wikipedia']]
 summits.to_csv('../pinnaclePoints.txt', index=False)
